6e41e4b19bc55c697a02c1c0c82b93b0eec16f26
Claista Guibert <gclaista@gmail.com>
2019-02-26 21:57:18 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(a,b) {
+  return (a + b)
+}
+function subtract(a,b) {
+  return (a - b)
+}
+function multiply(a,b) {
+  return (a * b)
+}
+function divide(a,b) {
+  return (a / b)
+}
+function inc(a) {
+  return (a + 1)
+}
+inc(a)
+function dec(a) {
+  return (a - 1)
+}
+dec(a)
+parseInt('a' , 10)
+function makeInt(a) {
+  return (a)
+}
+function makeInt() {
+  return (2328 * 0)
+}
